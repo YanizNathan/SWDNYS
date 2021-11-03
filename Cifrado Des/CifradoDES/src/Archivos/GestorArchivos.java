9574f867fb164975c554837e2b88869b3d99d798
YanizNathan <dyanizs1900@alumno.ipn.mx>
2021-11-02 20:03:55 -0600

    RSA

---
+++ Cifrado Des/CifradoDES/src/Archivos/GestorArchivos.java
@@ -0,0 +1,12 @@
+package Archivos;
+
+import Vistas.Cifrar;
+
+public class GestorArchivos {
+    
+    public static void main(String[] args) {
+        Cifrar cif = new Cifrar();
+        cif.setVisible(true);
+    }
+    
+}
